fa199a28d506702a1e4b6171c9e55ecf097da15f
Connor Cassidy 🤗 <connorcassidy.nos@gmail.com>
2022-04-19 15:41:41 -0400

    first public

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const siteMetadata = require('./site-metadata.json')
 
 module.exports = {
-    pathPrefix: '/',
+    pathPrefix: '/sebimor-oficial.github.io',
     siteMetadata: siteMetadata,
     plugins: [
         `gatsby-plugin-react-helmet`,
